284f6081aa6548cb9e4996822f6b6782a5209171
chris <christopherchateau@gmail.com>
2019-03-25 19:33:01 -0600

    Setup players and teams tables

--- db/migrations/20190325190208_initial.js
+++ db/migrations/20190325190208_initial.js
@@ -1,7 +1,25 @@
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable("players", table => {
+      table.increments("id").primary();
+      table.string("name");
+      table.string("bonus_points");
+    }),
+
+    knex.schema.createTable("teams", table => {
+      table.increments("id").primary();
+      table.integer("player_id").unsigned();
+      table.foreign("player_id").references("players.id");
+      table.string("name");
+      table.integer("points");
+      table.boolean("is_eliminated");
+    })
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable("players"),
+    knex.schema.dropTable("teams")
+  ]);
 };
