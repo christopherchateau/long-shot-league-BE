09a40a2b22f7fdbc8ac7ea17d3fd7ec1d2e1d91b
chris <christopherchateau@gmail.com>
2019-03-25 22:40:28 -0600

    Update server.js to allow CORS

--- server.js
+++ server.js
@@ -51,6 +51,12 @@ app.post("/api/v1/longshotleague/new_team", (request, response) => {
     });
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.listen(app.get("port"), () => {
   console.log(`visitor tracker is running on ${app.get("port")}.`);
 });
