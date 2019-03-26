03e33fe217d76cd12a638693c3ad1bd72ea9149f
chris <christopherchateau@gmail.com>
2019-03-25 19:59:07 -0600

    Install express and nodemon. Add get and post endpoints for players and teams

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const environment = process.env.NODE_ENV || "development";
+const config = require("./knexfile")[environment];
+const database = require("knex")(config);
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.set("port", process.env.PORT || 3001);
+
+app.get("/api/v1/longshotleague/players", (request, response) => {
+  database("players")
+    .select()
+    .then(visitor => {
+      response.status(200).json(visitor);
+    });
+});
+
+app.post("/api/v1/longshotleague/new_player", (request, response) => {
+  const newPlayer = request.body;
+
+  database("players")
+    .insert({ ...newPlayer }, "id")
+    .then(player => {
+      response.status(201).json("new player successfully added!");
+    })
+    .catch(error => {
+      response.status(500).json({ error: error.message });
+    });
+});
+
+app.get("/api/v1/longshotleague/teams", (request, response) => {
+  database("teams")
+    .select()
+    .then(visitor => {
+      response.status(200).json(visitor);
+    });
+});
+
+app.post("/api/v1/longshotleague/new_team", (request, response) => {
+  const newTeam = request.body;
+
+  database("teams")
+    .insert({ ...newTeam }, "id")
+    .then(team => {
+      response.status(201).json("new player successfully added!");
+    })
+    .catch(error => {
+      response.status(500).json({ error: error.message });
+    });
+});
+
+app.listen(app.get("port"), () => {
+  console.log(`visitor tracker is running on ${app.get("port")}.`);
+});
+
+module.exports = app;
