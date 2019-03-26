a5d230625548934f502e9a2cf5b19d3695e43ba5
chris <christopherchateau@gmail.com>
2019-03-25 19:00:02 -0600

    Setup knexfile.js, prepare for Heroku deployment

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+module.exports = {
+  development: {
+    client: "pg",
+    connection: "postgres://localhost/longShotLeague",
+    migrations: {
+      directory: "./db/migrations"
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: "./db/migrations"
+    },
+    useNullAsDefault: true
+  }
+};
