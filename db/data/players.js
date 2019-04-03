ce7031482284475b1740e51ef409b39e00f1ad08
chris <christopherchateau@gmail.com>
2019-04-02 18:32:20 -0600

    Remove hard-coded bonus totals from player.js, bonus data was moved to BE server

--- db/data/players.js
+++ db/data/players.js
@@ -1,82 +1,66 @@
 const players = [
   {
     name: "Chris",
-    bonus: 3,
     teams: ["Duke", "Syracuse", "New Mexico St", "Iona"]
   },
   {
     name: "Brewers",
-    bonus: 0,
     teams: ["Kentucky", "Seton Hall", "Mississippi St", "North Dakota St"]
   },
   {
     name: "Justin",
-    bonus: 0,
     teams: ["Murray St", "Yale", "Arizona St", "Fairleigh Dickinson"]
   },
   {
     name: "Brent",
-    bonus: 1,
     teams: ["Gonzaga", "Buffalo", "Maryland", "Gardner-Webb"]
   },
   {
     name: "Alex",
-    bonus: 2,
     teams: ["Houston", "Saint Louis", "Iowa", "Abilene-Christian"]
   },
   {
     name: "Matt",
-    bonus: 0,
     teams: ["Virginia", "Nevada", "Iowa St", "Bradley"]
   },
   {
     name: "Reed",
-    bonus: 2,
     teams: ["North Carolina", "Louisville", "St Mary's", "Colgate"]
   },
   {
     name: "Todd",
-    bonus: 2,
     teams: ["Wofford", "Belmont", "UC Irvine", "Montana"]
   },
   {
     name: "Laura",
-    bonus: 1,
     teams: ["Michigan St", "Kansas St", "Washington", "VCU"]
   },
   {
     name: "Scott",
-    bonus: 2,
     teams: ["Tennessee", "Marquette", "Ohio St", "Central Florida"]
   },
   {
     name: "Doherty",
-    bonus: 0,
     teams: ["Kansas", "Virginia Tech", "Florida", "Georgia St"]
   },
   {
     name: "Kevin",
-    bonus: 2,
     teams: ["Purdue", "Wisconsin", "Northern Kentucky", "Baylor"]
   },
   {
     name: "Kristin",
-    bonus: 0,
     teams: ["Michigan", "Auburn", "Minnesota", "Mississippi"]
   },
   {
     name: "Bmase",
-    bonus: 0,
     teams: ["Florida St", "Texas Tech", "Liberty", "Utah St"]
   },
   {
     name: "Fraske",
-    bonus: 1,
     teams: ["Villanova", "Cincinnati", "Vermont", "Old Dominion"]
   },
   {
     name: "Dan",
-    bonus: 0,
     teams: ["LSU", "Oregon", "Oklahoma", "Northeastern"]
   }
 ];
